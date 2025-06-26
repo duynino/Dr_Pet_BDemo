faa83e85ae40b290521504e46ff2b818da43a1e2
duynino <duynino2003@gmail.com>
2025-06-26 11:58:42 +0700

    Update outcome 2

--- src/main/java/com/example/dr_pet/config/SecurityConfig.java
+++ src/main/java/com/example/dr_pet/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // Domain frontend
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://mydrpet.vercel.app/")); // Domain frontend
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // Cho phép gửi cookie hoặc Authorization header
